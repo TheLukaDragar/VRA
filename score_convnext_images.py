96c41587b2459f36b75fddfe70d2c06bfd341388
TheLukaDragar <luka.dragarbusiness@gmail.com>
2023-08-20 21:36:34 +0200

    r

--- score_convnext_images.py
+++ score_convnext_images.py
@@ -399,7 +399,7 @@ if __name__ == "__main__":
     
 
     model = ConvNeXt(
-        og_path, model_name="convnext_xlarge_384_in22ft1k", loss=args.loss
+        og_path, model_name="convnext_xlarge_384_in22ft1k", dropout=0.1, loss="rmse"
     )
     # load checkpoint
     # get files in dir
